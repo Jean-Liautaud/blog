b94bedd516ae154f9ce6d042abacb8350d8f085b
Jean-Liautaud <45673353+Jean-Liautaud@users.noreply.github.com>
2018-12-10 11:02:15 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+// génération d'une constante HOME, qui contient l'url absolue vers la racine du site
+define("HOME", 'http://' . $_SERVER['SERVER_NAME'] . str_replace('index.php', '', $_SERVER['SCRIPT_NAME']));
+
+// fichier de connexion à pdo (normalement on  le range pas la mais c'est pour par perturber jojo)
+require_once "php\pdo.php";
+
+// 1. On choisit quelle page afficher (en général VUE\CONTROLLEUR) c'est le DISPATCHER
+require_once "php\dispatcher.php";
+
+// 2. On récupère les données à afficher et on reçoit les formulaires, c'est le CONTROLLEUR
+require_once "php\controller.php";
+
+// 3. Enfin on affiche le template et les données, c'est la VUE
+include "pages\layout.phtml";
